648c4488570909ee848f2302cfed04d9b9b9b123
serendipity <16611563+a_y_y@user.noreply.gitee.com>
2026-03-06 06:38:24 +0000

    feat:solve forward-dropout

--- musa_ext/kernels/math/musa_matmul_op.cc
+++ musa_ext/kernels/math/musa_matmul_op.cc
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 
+#include "../utils_op.h"
 #include "tensorflow/core/framework/bfloat16.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/op.h"
@@ -10,7 +11,6 @@
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/util/matmul_bcast.h"
-#include "../utils_op.h"
 
 namespace tensorflow {
 namespace musa {
@@ -54,7 +54,7 @@ class MusaMatMulOp : public MusaOpKernel {
       if (tf32_env) {
         return std::atoi(tf32_env) != 0;
       }
-      return true;  // Default: TF32 enabled for performance
+      return false;  // Default: TF32 enabled for performance
     }();
     tf32_enabled_ = tf32_enabled_global;
   }
@@ -122,10 +122,8 @@ class MusaMatMulOp : public MusaOpKernel {
       op.SetAlpha(1.0);
       op.SetBeta(0.0);
 
-      
       int64_t out_batch = bcast.output_batch_shape().num_elements();
 
-      
       auto ReshapeTo3D = [out_batch](mTensor& mt, const Tensor& t) {
         int64_t dims = t.dims();
         int64_t rows = t.dim_size(dims - 2);
@@ -134,14 +132,11 @@ class MusaMatMulOp : public MusaOpKernel {
 
         if (dims != 3) {
           if (batch == 1 && out_batch > 1) {
-            
             mt.SetNdInfo({out_batch, rows, cols}, {0, cols, 1});
           } else {
-            
             mt.SetNdInfo({batch, rows, cols}, {rows * cols, cols, 1});
           }
         } else if (dims == 3) {
-          
           if (batch == 1 && out_batch > 1) {
             mt.SetNdInfo({out_batch, rows, cols}, {0, cols, 1});
           }
@@ -150,12 +145,11 @@ class MusaMatMulOp : public MusaOpKernel {
 
       ReshapeTo3D(mt_a, in0);
       ReshapeTo3D(mt_b, in1);
-      
-      
+
       if (out_shape.dims() > 3) {
-         mt_out.SetNdInfo({out_batch, m, n}, {m * n, n, 1});
+        mt_out.SetNdInfo({out_batch, m, n}, {m * n, n, 1});
       } else if (out_shape.dims() == 2) {
-         mt_out.SetNdInfo({1, m, n}, {m * n, n, 1});
+        mt_out.SetNdInfo({1, m, n}, {m * n, n, 1});
       }
 
       status = op.Run(handle, mt_out, mt_a, mt_b);
