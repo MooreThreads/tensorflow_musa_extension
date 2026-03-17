c2b76fc3178020209023158acb823ac094e0c198
ronnie <ronnie@mt.com>
2026-03-17 17:33:18 +0800

    update kernel

--- musa_ext/kernels/nn/musa_sigmoid_calibration_op.cc
+++ musa_ext/kernels/nn/musa_sigmoid_calibration_op.cc
@@ -8,6 +8,10 @@
 namespace tensorflow {
 namespace musa {
 
+template <typename T>
+void LaunchSigmoidCalibrationKernel(const void*, const void*, void*, int,
+                                    musaStream_t);
+
 /**
  * MusaSigmoidCalibrationOp
  *
@@ -44,49 +48,13 @@ class MusaSigmoidCalibrationOp : public MusaOpKernel {
     MTOP_CHECK_OK_RUN(op.Run(handle, sigmoid_mt, in_mt), "Sigmoid Forward Run",
                       ctx);
 
-    // 2. Compute 1 - S
-    Tensor sub_output;
-    OP_REQUIRES_OK(
-        ctx, ctx->allocate_temp(input.dtype(), input.shape(), &sub_output));
-    mTensor sub_mt = CreateMTensor(sub_output);
-    Tensor ones;
-    OP_REQUIRES_OK(ctx,
-                   ctx->allocate_temp(input.dtype(), input.shape(), &ones));
-    mTensor ones_mt = CreateMTensor(ones);
-    SetOneFunctor::Compute<T>(ctx, &ones_mt);
-    mBinary sub_op;
-    MTOP_CHECK_OK(sub_op.SetMode(mBinary::Mode::SUB), "Set Sub", ctx);
-    MTOP_CHECK_OK_RUN(sub_op.Run(handle, sub_mt, ones_mt, sigmoid_mt),
-                      "Sub Run", ctx);
-
-    // 3. Compute Scale * (1 - S)
-    Tensor scale_mul_output;
-    OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                                           &scale_mul_output));
-    mTensor scale_mul_mt = CreateMTensor(scale_mul_output);
-    mBinary mul_op;
-    MTOP_CHECK_OK(mul_op.SetMode(mBinary::Mode::MUL), "Set Mul", ctx);
-    MTOP_CHECK_OK_RUN(mul_op.Run(handle, scale_mul_mt, scale_mt, sub_mt),
-                      "Mul Run", ctx);
-
-    // 4. Compute S + Scale * (1 - S)
-    Tensor denom_output;
-    OP_REQUIRES_OK(
-        ctx, ctx->allocate_temp(input.dtype(), input.shape(), &denom_output));
-    mTensor denom_mt = CreateMTensor(denom_output);
-    mBinary add_op;
-    MTOP_CHECK_OK(add_op.SetMode(mBinary::Mode::ADD), "Set Add", ctx);
-    MTOP_CHECK_OK_RUN(add_op.Run(handle, denom_mt, sigmoid_mt, scale_mul_mt),
-                      "Add Run", ctx);
-
-    // 5. Compute S / (S + Scale * (1 - S))
-    Tensor* output = nullptr;
+    // 2. Compute S / (S + Scale * (1 - S))
+    Tensor* output;
     OP_REQUIRES_OK(ctx, ctx->allocate_output(0, input.shape(), &output));
-    mTensor out_mt = CreateMTensor(*output);
-    mBinary div_op;
-    MTOP_CHECK_OK(div_op.SetMode(mBinary::Mode::DIV), "Set Div", ctx);
-    MTOP_CHECK_OK_RUN(div_op.Run(handle, out_mt, sigmoid_mt, denom_mt),
-                      "Div Run", ctx);
+    musaStream_t stream = GetMusaStreamByCtx(ctx);
+    LaunchSigmoidCalibrationKernel<T>(
+        sigmoid_mt.GetImpl(), scale_mt.GetImpl(), output->flat<T>().data(),
+        static_cast<int>(input.NumElements()), stream);
   }
 };
 
